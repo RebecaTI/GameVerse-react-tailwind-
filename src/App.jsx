ea1af0ef74b8c9523061e98f64971eb5bd09f967
RebecaTI <mebuffbeka@gmail.com>
2025-02-20 15:38:42 -0300

    feat(LandingPage): create main lading page component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import LandingPage from './components/LandingPage'
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen min-w-screen">
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App
